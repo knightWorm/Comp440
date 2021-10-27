78acc4986ed07f9bfca253265120bf535befc667
Nikko Austria <nikko.austria.646@my.csun.edu>
2021-10-27 11:45:14 -0700

    Added code to .js file for create account

--- static/sc.js
+++ static/sc.js
@@ -31,3 +31,83 @@ function refresh(){
 
 	window.location.href = window.location.href;
 }
+
+function checkUsername() {
+	var username = document.getElementById("createUsername").value;
+
+	if(username.length == 0){
+		return false;
+	}
+	/*else if(username == database value){
+		return false;
+	}*/
+	else{
+		return true;
+	}
+}
+
+function checkEmail() {
+	var email = document.getElementById("createEmail").value;
+
+	if(email.length == 0){
+		return false;
+	}
+	/*else if(username == database value){
+		return false;
+	}*/
+	else{
+		return true;
+	}
+}
+
+//take the ID's from html and set to these variables
+let createPassword = document.querySelector('#createPassword');
+let confirmPassword = document.querySelector('#confirmPassword');
+let result = document.querySelector('h3');
+
+//if passwords match say matching, else not matching
+//used in validAccount()
+function passwordMatch(){
+	result.innerText = createPassword.value == confirmPassword.value ? 'Password Matches' : 'Password Mismatch';
+	if(result.innerText == 'Password Matches'){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+createPassword.addEventListener('keyup', () => {
+	if (confirmPassword.value.length != 0) passwordMatch();
+})
+
+confirmPassword.addEventListener('keyup', passwordMatch);
+
+
+//for create account button, checks fields to see if username or email already exists in database
+//checks to see if the password fields match
+//if every field is valid it will create an account, add user data to database, then return user to sign in page
+function validateAccount(){
+	
+	if(checkUsername() == false){
+		alert("Username not valid");
+		return window.location.reload();
+	}
+	else if(checkEmail() == false){
+		alert("Email not valid");
+		return window.location.reload();
+	}
+	else if(passwordMatch() == false){
+		alert("Password not valid");
+		return window.location.reload();
+	}
+	else if(checkUsername() == true && checkEmail() == true && passwordMatch() == true){
+		alert("Account Created");
+		return window.location.replace("index.html");
+	}
+	else{
+		alert("Something not valid");
+		return window.location.reload();
+	}
+	
+}
