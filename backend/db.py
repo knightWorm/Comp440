9fbdb255fcca2bd8139be747e8213c5e86fa6dc7
Nikko Austria <nikko.austria.646@my.csun.edu>
2021-11-23 16:59:10 -0800

    Added the blog page

--- backend/db.py
+++ backend/db.py
@@ -5,7 +5,7 @@ class db:
         config = json.load(open('backend/private.json'))
         self.mydb = mysql.connector.connect(
             host = config['host'],
-            port = '8889',
+            port = '3006',
             user = config['user'],
             passwd = config['password'],
             database = config['database']
